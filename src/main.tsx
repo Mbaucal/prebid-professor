4db5101d06fe046fc2e6aa8eb8b2cad78f8b0813
Marko Baucal <63266577+Mbaucal@users.noreply.github.com>
2026-07-16 01:52:36 +0200

    style: load publisher hierarchy stylesheet

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client';
 import App from './App';
 import './styles.css';
 import './publisher-workflows.css';
+import './publisher-hierarchy.css';
 import './ad-units.css';
 import './bidders.css';
 import './imports.css';
