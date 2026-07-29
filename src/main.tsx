a5e4fd7d208233f1dab1b887426df0a8893b69f2
Marko Baucal <63266577+Mbaucal@users.noreply.github.com>
2026-07-30 00:59:24 +0200

    Remove experimental ads.txt duplicate UI from preview

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,6 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App';
-import AdsTxtRequirementDuplicateGuard from './components/AdsTxtRequirementDuplicateGuard';
 import AuthAccount from './components/AuthAccount';
 import TesseraBranding from './components/TesseraBranding';
 import './styles.css';
@@ -50,7 +49,6 @@ createRoot(root).render(
   <StrictMode>
     <TesseraBranding />
     <App />
-    <AdsTxtRequirementDuplicateGuard />
     <AuthAccount />
   </StrictMode>,
 );
