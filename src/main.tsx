ad7abec10c4e6f81145d7a8a86aef3cec8112235
Marko Baucal <63266577+Mbaucal@users.noreply.github.com>
2026-08-03 18:15:27 +0200

    Enable collapsible ads.txt requirements in Monitoring preview

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App';
+import AdsTxtRequirementsCollapse from './components/AdsTxtRequirementsCollapse';
 import AuthAccount from './components/AuthAccount';
 import TesseraBranding from './components/TesseraBranding';
 import './styles.css';
@@ -29,6 +30,7 @@ import './export.css';
 import './mockup-builder.css';
 import './debug-console.css';
 import './ads-txt.css';
+import './ads-txt-requirements-collapse.css';
 import './global-workspaces.css';
 import './monitoring-readonly.css';
 import './monitoring-email-preview.css';
@@ -49,6 +51,7 @@ createRoot(root).render(
   <StrictMode>
     <TesseraBranding />
     <App />
+    <AdsTxtRequirementsCollapse />
     <AuthAccount />
   </StrictMode>,
 );
